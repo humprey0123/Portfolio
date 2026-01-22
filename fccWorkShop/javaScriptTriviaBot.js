33c2fa97206dd25332f599d1f06475927512962e
Ian Humprey <ian.humprey09@gmail.com>
2026-01-22 14:24:35 +0800

    Building Trivia Bot

---
+++ fccWorkShop/javaScriptTriviaBot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Fern";
+let botLocation = "Southern Lands";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is" + " " + botName + " " + "and I live on" + " " + botLocation +".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Old but gold";
+
+codingFact = favoriteLanguage + " is " + codingFact;
+
+console.log(codingFact);
+
+codingFact += "... For real. " + favoriteLanguage + " is.";
+console.log(codingFact);
+
+codingFact += favoriteLanguage +  " is the foundation for web development.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName +" from " + botLocation + ".");

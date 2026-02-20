0b11a38080fd972b18e706952bbc03ae6caa462f
Ian Humprey <ian.humprey09@gmail.com>
2026-02-20 16:55:39 +0800

    Lunch Picker Program(not finished)

--- fcc/js/fccJsLab/1Array.js
+++ fcc/js/fccJsLab/1Array.js
@@ -25,3 +25,48 @@ function removeLunchToStart() {
 removeLunchToEnd()
 removeLunchToStart()
 console.log("Lunch: "+lunches.join(", "));
+
+
+
+
+
+// 2nd attempt
+let lunches = [];
+
+function addLunchToEnd(lunchesArr, lunchItem) {
+  lunchesArr.push(lunchItem)
+  console.log(`${lunchItem} added to the end of the lunch menu.`)
+  return lunchesArr;
+}
+function addLunchToStart(lunchesArr, lunchItem) {
+  lunchesArr.unshift(lunchItem)
+  console.log(`${lunchItem} added to the start of the lunch menu.`)
+  return lunchesArr;
+}
+
+function removeLastLunch(lunchesArr) {
+  if (lunchesArr.length > 0) {
+    console.log(lunchesArr.pop() + " removed from the end of the lunch menu.")
+    return lunchesArr
+  } else {
+    console.log("No lunches to remove.");
+  }
+}
+function removeFirstLunch(lunchesArr) {
+  if (lunchesArr.length > 0) {
+    console.log(lunchesArr.shift() + " removed from the start of the lunch menu.")
+    return lunchesArr
+  } else {
+    console.log("No lunches to remove.");
+  }
+}
+function getRandomLunch(Param) {
+  if (Param == 0) {
+    console.log("No lunches available.")
+  } else if (Param >= 1) {
+    Param.length *
+  }
+}
+lunches.push("Borger saka fries", "Hotdog");
+console.log(addLunchToEnd(["Pizza", "Tacos"], "Burger"))
+console.log(removeLastLunch(lunches))
